d2a3c3e8628c0fe9c7fb1bc1a52ef7f6fb3c3803
Peter A. H. Peterson <pedro@tastytronic.net>
2017-04-12 14:23:30 -0500

    Adding code with one-cell sprites - prep for change
    Changing to an approach with a larger map where, e.g., curves like the wall "caps" are made with two quarter curves. I'll be removing a large number of constants and tiles.

--- game.py
+++ game.py
@@ -10,34 +10,21 @@ from CYLGame import PanelBorder
 DEBUG = False
 
 
-class Ski(Game):
-    MAP_WIDTH = 60
-    MAP_HEIGHT = 30
-    SCREEN_WIDTH = 60
+class PacBot(Game):
+    MAP_WIDTH = 22
+    MAP_HEIGHT = 22
+    SCREEN_WIDTH = MAP_WIDTH + 2
     SCREEN_HEIGHT = MAP_HEIGHT + 6
     MSG_START = 20
     MAX_MSG_LEN = SCREEN_WIDTH - MSG_START - 1
     CHAR_WIDTH = 16
     CHAR_HEIGHT = 16
-    GAME_TITLE = "Ski"
+    GAME_TITLE = "Pac-Bot"
     CHAR_SET = "terminal16x16_gs_ro.png"
 
     SENSE_DIST = 20
 
-    LEVELUP_RESPONSES = ["The forest seems to be getting more dense!", "Are there more trees here or what?", "Watch out!", "Better pay attention!"]
 
-    ROBOT_CRASH_RESPONSES = ["OOF!", "OWWWIE!", "THAT'S GONNA LEAVE A MARK!", "BONK!"]
-    ROBOT_HEART_RESPONSES = ["Wow, I feel a lot better!", "Shazam!", "That's the ticket!", "Yes!!!"]
-    ROBOT_COIN_RESPONSES = ["Cha-ching!", "Badabing!", "Bling! Bling!", "Wahoo!"]
-    ROBOT_FLYING_RESPONSES = ["I'm free as a bird now!", "It's a bird, it's a plane...", "Cowabunga!"]
-
-    NUM_OF_ROCKS_START = 30
-    NUM_OF_TREES_START = 30
-    NUM_OF_SPIKES_START = 30
-    NUM_OF_SNOWMAN_START = 30
-    NUM_OF_COINS_START = 1
-    NUM_OF_HEARTS_START = 1
-    NUM_OF_JUMPS_START = 1
     MAX_TURNS = 500
     MAX_FLYING = 10
     FLYING_POINTS = 5
@@ -47,18 +34,29 @@ class Ski(Game):
 
     PLAYER = '@'
     EMPTY = '\0'
-    HEART = chr(3)
-    COIN = chr(4)
-    ROCK = chr(15)
-    SPIKE = chr(16)
-    SNOWMAN = chr(17)
-    TRACKS = chr(29)
-    TREE = chr(30)
-    JUMP = chr(31)
-    DEAD = chr(1)
-    FLY = chr(2)
-    CRASH = chr(8)
-    HOUSE = chr(9)
+    LWALL = chr(255)
+    UWALL = chr(254)
+    DWALL = chr(253)
+    RWALL = chr(252)
+    UCAP = chr(251)
+    DCAP = chr(250)
+    RCAP = chr(249)
+    LCAP = chr(248)
+    ULCOR = chr(247)
+    URCOR = chr(246)
+    BRCOR = chr(245)
+    BLCOR = chr(244)
+    URXCOR = chr(243)
+    BRXCOR = chr(242)
+    BLXCOR = chr(241)
+    ULXCOR = chr(240)
+    LRWALL = chr(239)
+    UDWALL = chr(238)
+    FULL = chr(224)
+    DOT = chr(225)
+    POWER = chr(226)
+    DOOR = chr(227)
+    
 
     def __init__(self, random):
         self.sensor_coords = [] # variables for adjustable sensors from LP
@@ -87,28 +85,43 @@ class Ski(Game):
                             border=PanelBorder.create(bottom="-"))
 
         self.panels += [self.map]
-        self.place_objects(self.TREE, self.NUM_OF_ROCKS_START)
-        self.place_objects(self.ROCK, self.NUM_OF_TREES_START)
-        self.place_objects(self.SNOWMAN, self.NUM_OF_SNOWMAN_START)
-        self.place_objects(self.COIN, self.NUM_OF_COINS_START)
-        self.place_objects(self.HEART, self.NUM_OF_HEARTS_START)
-        self.place_objects(self.JUMP, self.NUM_OF_JUMPS_START)
 
-        # make a clearing for the player
-        for y in range(8):
-            for x in range(self.MAP_WIDTH):
-                self.map[(x, self.MAP_HEIGHT - 1 - y)] = self.EMPTY
+        mapmap = {'Q': self.LWALL,
+                  'W': self.RWALL,
+                  'E': self.UWALL,
+                  'R': self.DWALL,
+                  'A': self.LCAP,
+                  'S': self.RCAP,
+                  'D': self.UCAP,
+                  'F': self.DCAP,
+                  'Z': self.BLCOR,
+                  'X': self.ULCOR,
+                  'C': self.BRCOR,
+                  'V': self.URCOR,
+                  '1': self.BLXCOR,
+                  '2': self.ULXCOR,
+                  '3': self.BRXCOR,
+                  '4': self.URXCOR,
+                  'U': self.FULL,
+                  'I': self.DOT,
+                  'O': self.POWER,
+                  'P': self.DOOR,
+                  'H': self.LRWALL,
+                  'J': self.UDWALL}
 
-        # place player
-        self.map[(self.player_pos[0], self.player_pos[1])] = self.PLAYER
-
-        # place decorative trees
-        self.map[(self.player_pos[0] + 5, self.player_pos[1])] = self.TREE
-        self.map[(self.player_pos[0] - 5, self.player_pos[1])] = self.TREE
-
-        # place initial hearts
-        self.map[(self.player_pos[0], self.player_pos[1] - 2)] = self.HEART
-        self.map[(self.player_pos[0], self.player_pos[1] - 3)] = self.HEART
+        # open map file
+        x = 0
+        y = 0
+        with open("map.txt") as f:
+            for line in f:
+                x = 0
+                for char in line:
+                    if char != '\n':
+                        self.map[(x, y)] = mapmap[char]
+                    x += 1
+                y += 1
+        
+        self.map[(9,15)] = self.PLAYER
 
         if DEBUG:
             print(self.get_vars_for_bot())  # need sensors before turn
@@ -126,30 +139,6 @@ class Ski(Game):
                 # we can replace objects that exist
                 self.map[(x, y)] = char
                 placed_objects += 1
-
-    def make_new_row(self):
-        for x in range(self.MAP_WIDTH):
-            here = self.random.randint(0, self.MAX_TURNS)
-            if here <= self.turns:
-                which = self.random.randint(0, 2)
-                if which == 0:
-                    self.map[(x, 0)] = self.ROCK
-                elif which == 1:
-                    self.map[(x, 0)] = self.TREE
-                elif which == 2:
-                    self.map[(x, 0)] = self.SNOWMAN
-
-        if self.random.randint(0, 100) > 33:
-            self.map[(self.random.randint(0, self.MAP_WIDTH - 1), 0)] = self.HEART
-
-        if self.random.randint(0, 100) > 33:
-            self.map[(self.random.randint(0, self.MAP_WIDTH - 1), 0)] = self.COIN
-
-        if self.random.randint(0, 100) > 33:
-            self.map[(self.random.randint(0, self.MAP_WIDTH - 1), 0)] = self.JUMP
-
-        if self.random.randint(0, self.HOUSE_ODDS) == 1:
-            self.map[(self.random.randint(0, self.MAP_WIDTH - 1), 0)] = self.HOUSE
 
     def save_object(self, obj):
         self.saved_object = obj
@@ -188,16 +177,6 @@ class Ski(Game):
                     self.map[(self.player_pos[0] + x, self.player_pos[1] + y)] = self.TRACKS
 
 
-    def shift_map(self):
-        # shift all rows down
-        dx = (self.MAP_WIDTH / 2) - self.player_pos[0]
-        self.map.shift_all((dx, 1), wrap_x=True)
-        self.player_pos[0] += dx
-
-        self.make_new_row()
-
-        self.restore_object_tracks()
-
     def handle_key(self, key):
         self.turns += 1
         if self.flying > 0:
@@ -232,75 +211,15 @@ class Ski(Game):
         self.last_move = key # save last move for saved_object restoration
 
         # shift the map
-        self.shift_map()
         
         self.colliding = False  # reset colliding variable
 
         # check for various types of collisions (good and bad)
-        if self.map[(self.player_pos[0], self.player_pos[1])] == self.ROCK:
-            self.save_object(self.ROCK)
-            if self.flying == 0:
-                self.colliding = True
-                self.hp -= 10
-                self.msg_panel += [self.random.choice(list(set(self.ROBOT_CRASH_RESPONSES) - set(self.msg_panel.get_current_messages())))]
-
-        elif self.map[(self.player_pos[0], self.player_pos[1])] == self.TREE:
-            self.save_object(self.TREE)
-            if self.flying == 0:
-                self.colliding = True
-                self.hp -= 2
-                self.msg_panel += [self.random.choice(list(set(self.ROBOT_CRASH_RESPONSES) - set(self.msg_panel.get_current_messages())))]
-
-        elif self.map[(self.player_pos[0], self.player_pos[1])] == self.SNOWMAN:
-            if self.flying == 0:
-                self.colliding = True
-                self.hp -= 1
-                self.msg_panel += [self.random.choice(list(set(self.ROBOT_CRASH_RESPONSES) - set(self.msg_panel.get_current_messages())))]
-            else:
-                self.save_object(self.SNOWMAN) # flying over snowmen is nondestructive
-
-        elif self.map[(self.player_pos[0], self.player_pos[1])] == self.HEART:
-            if self.flying == 0:
-                if self.hp < 10:
-                    self.hp += 1
-                    self.msg_panel += [self.random.choice(list(set(self.ROBOT_HEART_RESPONSES) - set(self.msg_panel.get_current_messages())))]
-                else:
-                    self.msg_panel += ["Your HP is already full!"]
-            else:
-                self.save_object(self.HEART)
-
-        elif self.map[(self.player_pos[0], self.player_pos[1])] == self.HOUSE:
-            if self.flying == 0:
-                self.hp = 10
-                self.msg_panel += ["This cabin was very refreshing!"]
-            else:
-                self.save_object(self.HOUSE)
-
-
-        elif self.map[(self.player_pos[0], self.player_pos[1])] == self.COIN:
-            if self.flying == 0:
-                self.score += self.COIN_POINTS
-                self.msg_panel += [self.random.choice(list(set(self.ROBOT_COIN_RESPONSES) - set(self.msg_panel.get_current_messages())))]
-            else:
-                self.save_object(self.COIN)
-
-        elif self.map[(self.player_pos[0], self.player_pos[1])] == self.JUMP:
-            if self.flying == 0:
-                self.save_object(self.JUMP)
-                self.flying += self.random.randint(2, self.MAX_FLYING)
-                self.msg_panel += [self.random.choice(list(set(self.ROBOT_FLYING_RESPONSES) - set(self.msg_panel.get_current_messages())))]
-            else:
-                self.save_object(self.JUMP)
-
-        # draw player
         if self.flying < 1:
             if self.colliding:
-                self.map[(self.player_pos[0], self.player_pos[1])] = self.CRASH
+                self.map[(self.player_pos[0], self.player_pos[1])] = self.FULL
             else:
                 self.map[(self.player_pos[0], self.player_pos[1])] = self.PLAYER
-
-        else:
-            self.map[(self.player_pos[0], self.player_pos[1])] = self.FLY
 
         # vars should be gotten at the end of handle_turn, because vars
         # affect the *next* turn...
@@ -322,15 +241,8 @@ class Ski(Game):
             self.sensor_coords.append((state.get(x_name, "0"), state.get(y_name, "0")))
 
     def get_vars_for_bot(self):
-        bot_vars = {"jump_x": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.JUMP, default=(0, 0))[0],
-                    "jump_y": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.JUMP, default=(0, 0))[1],
-                    "heart_x": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.HEART, default=(0, 0))[0],
-                    "heart_y": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.HEART, default=(0, 0))[1],
-                    "coin_x": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.COIN, default=(0, 0))[0],
-                    "coin_y": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.COIN, default=(0, 0))[1],
-                    "house_x": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.HOUSE, default=(0, 0))[0],
-                    "house_y": self.map.get_x_y_dist_to_foo(tuple(self.player_pos), self.HOUSE, default=(0, 0))[1],
-                    "hp": 0, "flying": 0, "s1": 0, "s2": 0, "s3": 0, "s4": 0, "s5": 0, "s6": 0, "s7": 0}
+
+        bot_vars = {}
 
         # go through self.sensor_coords and retrieve the map item at the
         # position relative to the player
@@ -365,30 +277,12 @@ class Ski(Game):
     def get_move_consts():
         consts = Game.get_move_consts()
         consts.update({"teleport": ord("t")})
-        consts.update({"heart": ord(Ski.HEART)})
-        consts.update({"coin": ord(Ski.COIN)})
-        consts.update({"rock": ord(Ski.ROCK)})
-        consts.update({"spikes": ord(Ski.SPIKE)})
-        consts.update({"snowman": ord(Ski.SNOWMAN)})
-        consts.update({"tracks": ord(Ski.TRACKS)})
-        consts.update({"tree": ord(Ski.TREE)})
-        consts.update({"jump": ord(Ski.JUMP)})
-        consts.update({"house": ord(Ski.HOUSE)})
         return consts
 
     @staticmethod
     def get_move_names():
         names = Game.get_move_names()
         names.update({ord("t"): "teleport"})
-        names.update({ord(Ski.HEART): "heart"})
-        names.update({ord(Ski.COIN): "coin"})
-        names.update({ord(Ski.ROCK): "rock"})
-        names.update({ord(Ski.SPIKE): "spikes"})
-        names.update({ord(Ski.SNOWMAN): "snowman"})
-        names.update({ord(Ski.TRACKS): "tracks"})
-        names.update({ord(Ski.TREE): "tree"})
-        names.update({ord(Ski.JUMP): "jump"})
-        names.update({ord(Ski.HOUSE): "house"})
         return names
 
     def get_score(self):
@@ -412,7 +306,6 @@ class Ski(Game):
         # Update Status
         self.status_panel["Score"] = self.score
         self.status_panel["Move"] = str(self.turns) + " of " + str(self.MAX_TURNS)
-        self.status_panel["HP"] = self.HEART * self.hp
 
         for panel in self.panels:
             panel.redraw(libtcod, console)
@@ -420,4 +313,4 @@ class Ski(Game):
 
 if __name__ == '__main__':
     from CYLGame import run
-    run(Ski)
+    run(PacBot)
